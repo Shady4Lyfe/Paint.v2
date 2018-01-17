0710fdda5f01ee3bbbfc9f8d6a2f1330d6a3d9f6
Shady4Lyfe <32204463+Shady4Lyfe@users.noreply.github.com>
2018-01-16 23:13:34 -0700

    dethzdv s

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 $("#penColor").change(updatePenColor);
+$("#bgColor").change(updateBgColor);
 $("#penSize").change(updatePenSize);
 $("#clearC").click(clearCanvas);
+$("#eraser").click(eraser);
+$("#device").click(device);
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -11,6 +14,11 @@ function updatePenColor() {
   stroke(newPenColor);
 }
 
+function updateBgColor()  {
+  let newBgColor = $("#bgColor").val();
+  background(newBgColor);
+}
+
 function updatePenSize() {
   let newPenSize = $("#penSize").val();
   strokeWeight(newPenSize);
@@ -18,6 +26,16 @@ function updatePenSize() {
 
 function clearCanvas() {
   clear();
+}
+
+function device()  {
+  let newPenColor = $("#penColor").val();
+  stroke(newPenColor);
+}
+
+function eraser() {
+  stroke("#FFFFF");
+  $("#eraser").css("cursor", "url[images/Eraser_Icon.png]");
 }
 
 function draw() {
